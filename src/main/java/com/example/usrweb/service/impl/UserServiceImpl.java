b8e492663c45b2908a26d5434a6798380dad09a4
furuifr <565668066@qq.com>
2019-09-10 14:52:52 +0800

    Merge branch 'springbootZDJ' of https://github.com/lc1997622/USRWeb into furui
    # Conflicts:
    #	src/main/java/com/example/usrweb/service/impl/UserServiceImpl.java

--- src/main/java/com/example/usrweb/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/usrweb/service/impl/UserServiceImpl.java
@@ -16,24 +16,23 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.lang.reflect.Field;
 
-/**   
+/**
  * <p>自动生成工具：mybatis-dsc-generator</p> 
- * 
+ *
  * <p>说明： 用户服务实现层</P>
  * @version: V1.0
  * @author: LiChao
- * 
+ *
  */
 @Service
 public class UserServiceImpl  extends ServiceImpl<UserDao, User> implements UserService  {
 
-    public boolean uploadExcel(MultipartFile multipartFile) throws Exception{
+    public boolean uploadFile(MultipartFile multipartFile, String parentPath) throws Exception{
         if (multipartFile.isEmpty()){
             return false;
         }
         // 得到文件在客户机的文件名
         String fileName = multipartFile.getOriginalFilename();
-        String parentPath = "C:/Users/Administrator/Desktop/软件工程专业实训2019/project/USRWeb/src/main/resources/static/images/news";
         String excelPath = parentPath + "/" + fileName;
         File file = new File(excelPath);
         // 文件父目录
